04ec2ade60fdfbb2c267d5dbb4e6f214295663ee
rootdanley <danleyalves@hotmail.com>
2023-07-19 22:10:46 -0300

    add metodo

--- OOP/src/With_OPP.java
+++ OOP/src/With_OPP.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class With_OPP {
+import entities.Triangle;
+
+import java.util.Scanner;
+
+public class With_OPP {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X:");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of tringle Y:");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.b = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle X area: %.4f%n", areaY);
+		
+		if(areaX > areaY){
+			System.out.println("Larger area: X");
+		}
+		else{
+			System.out.println("Larger area: Y");
+		}
+		
+		sc.close();
+	}
 }
--- OOP/src/entities/Triangle.java
+++ OOP/src/entities/Triangle.java
@@ -1,2 +1,12 @@
-package entities;public class Triangle {
+package entities;
+
+public class Triangle {
+	public double a;
+	public double b;
+	public double c;
+	
+	public double area(){
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
