9cb233850c3e496abd42181793b6bcf14a242067
rootdanley <danleyalves@hotmail.com>
2023-07-21 09:12:45 -0300

    Update: Organizacao de archives

--- exemplo-02/src/Programa.java
+++ exemplo-02/src/Programa.java
@@ -1,3 +1,4 @@
+import entities.Product;
 import java.util.Scanner;
 
 public class Programa {
@@ -5,7 +6,17 @@ public class Programa {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		Product product = new Product(); // instanciado o objeto
 		
+		System.out.println("Enter product data:");
+		System.out.print("Name:");
+		product.name = sc.nextLine();
+		System.out.print("Price:");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock:");
+		product.quantity = sc.nextInt();
+		
+		System.out.print(product.getClass());
 		
 		sc.close();
 	}
